852fec733e8a7c6e40e3dd44f47c85117a335651
beyondhealthsystems <steven@beyond-ai.com>
2026-01-08 17:56:27 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -94,11 +94,10 @@ export default function Home() {
       marginTop: 18,
     },
     img: {
-  display: "block",
-  width: "70%",
-  height: "auto",
-  margin: "0 auto",
-},
+      display: "block",
+      width: "100%",
+      height: "auto",
+    },
     imgCaption: {
       padding: "12px 14px",
       fontSize: 14,
